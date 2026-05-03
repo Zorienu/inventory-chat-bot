74f7dd6a874109385d909ff0907b36fe33caa40b
Jasyd <jasydavid45@gmail.com>
2026-05-03 16:38:56 -0500

    clean up

--- src/index.ts
+++ src/index.ts
@@ -13,26 +13,9 @@ interface SendMessageResponse {
   messages: { id: string }[];
 }
 
-async function sendWhatsAppMessage(to: string, message: string) {
+async function sendTextMessage(to: string, message: string) {
   try {
     const url = `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`;
-
-    // Plantilla
-    const dataPlantilla = {
-      messaging_product: "whatsapp",
-      to, // Formato: 573001234567
-      type: "template",
-      text: {
-        preview_url: false,
-        body: message,
-      },
-      template: {
-        name: "hello_world",
-        language: {
-          code: "en_US",
-        },
-      },
-    };
 
     const data = {
       messaging_product: "whatsapp",
@@ -44,16 +27,6 @@ async function sendWhatsAppMessage(to: string, message: string) {
       },
     };
 
-    // {
-    //   "to": "573157332828",
-    // "template": {
-    //   "name": "hello_world",
-    //   "language": {
-    //     "code": "en_US"
-    //   }
-    // }
-    // }
-
     const response = await axios.post<SendMessageResponse>(url, data, {
       headers: {
         Authorization: `Bearer ${WHATSAPP_TOKEN}`,
@@ -64,15 +37,15 @@ async function sendWhatsAppMessage(to: string, message: string) {
     console.log("Mensaje enviado con éxito:", response.data.messages[0].id);
   } catch (error) {
     console.error("Error enviando mensaje:", error);
-    console.log("@@error", (error as any).response.data.error);
   }
 }
 
-// Ejemplo de uso
-// sendWhatsAppMessage('57310XXXXXXX', '¡Hola! Soy tu bot de inventario.');
-//
+const whatsapp = {
+  sendTextMessage
+}
+
 async function main() {
-  await sendWhatsAppMessage(
+  await whatsapp.sendTextMessage(
     "+573157332828",
     "¡Hola! Soy tu bot de inventario.",
   );
