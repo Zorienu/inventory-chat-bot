55c7d5323da6e81475095be19b30fd7191db0909
Jasyd <jasydavid45@gmail.com>
2026-05-03 17:17:01 -0500

    move whatsapp logic to its own module

---
+++ src/whatsapp/index.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
+const VERSION = "v25.0";
+
+interface SendMessageResponse {
+  messaging_product: string;
+  contacts: { input: string; wa_id: string }[];
+  messages: { id: string }[];
+}
+
+async function sendTextMessage(to: string, message: string) {
+  try {
+    const url = `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`;
+
+    const data = {
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to,
+      type: "text",
+      text: {
+        body: message,
+      },
+    };
+
+    const response = await axios.post<SendMessageResponse>(url, data, {
+      headers: {
+        Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Mensaje enviado con éxito:", response.data.messages[0].id);
+  } catch (error) {
+    console.error("Error enviando mensaje:", error);
+  }
+}
+
+const whatsapp = {
+  sendTextMessage
+}
